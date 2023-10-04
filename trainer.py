6178ec23a71bba003ea91474443f455fa1de2816
Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
2023-10-04 12:31:23 +0200

    wandb logging and allow starting from pretrained model

--- trainer.py
+++ trainer.py
@@ -18,25 +18,7 @@ def compute_metrics(eval_preds):
     acc = np.mean(np.sum((preds==labels) & (labels!=-100), axis=-1) / np.sum(labels != -100, axis=-1))
     return {"acc": acc}
 
-if __name__=='__main__':
-    print("Starting training")
-
-    parser = argparse.ArgumentParser(description='Training script.')
-    parser.add_argument('--config', action = 'append', type = str, help = 'Configuration', required=True)
-    args = parser.parse_args()
-    cfg = pconfig.load_config(args.config)
-
-    if hasattr(cfg, "wandb"):
-        wandb.login(key=cfg.wandb.api_key)
-        wandb.init(entity=cfg.wandb.entity, project=cfg.wandb.project)
-        wandb.config.update({"pconfig": pconfig.namespace_to_dict(cfg)})
-        report_to = "wandb"
-    else:
-        report_to = "none"
-
-    torch.manual_seed(0) # Needed to make encodec model weights deterministic and hence reuse cache
-    ds = prepare_data.lj_speech_dataset(cfg.prepare_data)
-
+def make_model(cfg):
     tokenizer = AutoTokenizer.from_pretrained("google/byt5-small")
     enc_config = AutoConfig.from_pretrained(
         "gpt2",
@@ -58,6 +40,32 @@ if __name__=='__main__':
     model = EncoderDecoderModel(config=config)
     model.config.decoder_start_token_id = prepare_data.TOK_BOS
     model.config.pad_token_id = prepare_data.TOK_EOS
+    return model
+
+if __name__=='__main__':
+    print("Starting training")
+
+    parser = argparse.ArgumentParser(description='Training script.')
+    parser.add_argument('--config', action = 'append', type = str, help = 'Configuration', required=True)
+    parser.add_argument('--with-model', action = 'store', type = str, help = 'Pretrained model')
+    args = parser.parse_args()
+    cfg = pconfig.load_config(args.config)
+
+    if hasattr(cfg, "wandb"):
+        wandb.login(key=cfg.wandb.api_key)
+        wandb.init(entity=cfg.wandb.entity, project=cfg.wandb.project)
+        wandb.config.update({"pconfig": pconfig.namespace_to_dict(cfg)})
+        report_to = "wandb"
+    else:
+        report_to = "none"
+
+    torch.manual_seed(0) # Needed to make encodec model weights deterministic and hence reuse cache
+    ds = prepare_data.lj_speech_dataset(cfg.prepare_data)
+
+    if not args.with_model:
+        model = make_model(cfg)
+    else:
+        model = EncoderDecoderModel.from_pretrained(args.with_model)
 
     args = Seq2SeqTrainingArguments(
         pconfig.model_path(cfg.model.name, None),
@@ -66,6 +74,7 @@ if __name__=='__main__':
         **cfg.training.args.__dict__,
     )
 
+    tokenizer = AutoTokenizer.from_pretrained("google/byt5-small")
     data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True)
 
     trainer = Seq2SeqTrainer(
